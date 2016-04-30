d0867a0a382b812e2875cef75b83ef12765e1f3c
Jason Cheng <jasonvitagen@gmail.com>
2016-04-30 18:09:22 +0800

    Remove Old Files

--- server-src/routes/noScriptItemRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import restfulApi from '../modules/restfulApi';
-
-const router = express.Router();
-
-router.all('/item/:id?', restfulApi.restful('NoScript.Item'));
-
-export default router;
--- server-src/routes/noScriptItemsRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import restfulApi from '../modules/restfulApi';
-
-const router = express.Router();
-
-router.all('/items', restfulApi.restful('NoScript.Items'));
-
-export default router;
